5b1dacbfe2da3068a6690211f953cfc7c7070021
hieugiddy <hieugiddy.pro@gmail.com>
2021-05-01 22:35:58 +0700

    b

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -8,6 +8,7 @@ router.use("/", require(__dirname + "/admin"));
 router.use("/account", require(__dirname + "/account"));
 router.use("/api/account", require("../api/account"));
 router.use("/api/doitac", require("../api/doitac"));
+router.use("/api/hethong", require("../api/hethong"));
 
 module.exports=router;
 
