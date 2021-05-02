7854fa2c5012a785e93503d191da161baaef46b1
hieugiddy <hieugiddy.pro@gmail.com>
2021-05-02 20:40:46 +0700

    upload

--- apps/api/account.js
+++ apps/api/account.js
@@ -11,7 +11,7 @@ const Storage = multer.diskStorage({
         callback(null, appRoot+'/public/img');
     },
     filename(req, file, callback) {
-        callback(null, '${file.fieldname}_${Date.now()}_${file.originalname}');
+        callback(null, file.fieldname+'_'+Date.now()+'_'+file.originalname);
     },
 });
 const upload = multer({ storage: Storage });
@@ -82,7 +82,7 @@ router.route("/kiem-tra-dang-nhap")
         }
 
     });
-router.get('/upload-avatar', upload.array('photo', 3), (req, res) => {
+router.post('/upload-avatar', upload.array('photo', 3), (req, res) => {
     console.log('file', req.files);
     console.log('body', req.body);
     res.status(200).json({
