0b406e1df13d22030f401a4b9511a0b4aed21c9f
hieugiddy <hieugiddy.pro@gmail.com>
2021-05-02 21:54:27 +0700

    edittk

--- apps/api/account.js
+++ apps/api/account.js
@@ -5,17 +5,19 @@ var router = express.Router();
 var UserModel = require("../models/user");
 var UserHelper = require("../helpers/account");
 const multer = require('multer');
-var config=require('config');
+var config = require('config');
 
 const Storage = multer.diskStorage({
     destination(req, file, callback) {
-        callback(null, appRoot+'/public/img');
+        callback(null, appRoot + '/public/img');
     },
     filename(req, file, callback) {
-        callback(null, file.fieldname+'_'+Date.now()+'_'+file.originalname);
+        callback(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
     },
 });
 const upload = multer({ storage: Storage });
+
+
 router.route("/dang-ky")
     .post(function (req, res) {
         var user = req.body;
@@ -85,9 +87,9 @@ router.route("/kiem-tra-dang-nhap")
     });
 router.post('/upload-avatar', upload.array('photo', 3), (req, res) => {
     let file = req.files;
-    let id= req.body.id;
-    
-    let avatar = config.get('server.link')+'/static/img/'+file[0].filename;
+    let id = req.body.id;
+
+    let avatar = config.get('server.link') + '/static/img/' + file[0].filename;
     let result = UserModel.uploadAvatar(id, avatar);
 
     result.then(function (data) {
@@ -97,6 +99,65 @@ router.post('/upload-avatar', upload.array('photo', 3), (req, res) => {
     }).catch(function (err) {
         res.json({ message: err });
     })
-   
+
 });
+router.route("/cap-nhat-tai-khoan")
+    .post(function (req, res) {
+        var user = req.body;
+        try {
+            if (!UserHelper.telephoneValidation(user.SoDienThoai))
+                throw 'Số điện thoại sai định dạng';
+
+            var result = UserModel.updateUser(user.ID_TaiKhoan, user);
+
+            if (!result)
+                res.json({ "Messenger": "Đã có lỗi xảy ra" });
+            else
+                result.then(function (data) {
+                    res.json({ "Messenger": "Thay đổi thông tin tài khoản thành công" });
+                }).catch(function (err) {
+                    res.json({ "Messenger": "Thông tin nhập vào không chính xác" });
+                })
+        }
+        catch (e) {
+            res.json({ "Messenger": e });
+        }
+    });
+
+router.route("/doi-mat-khau")
+    .post(function (req, res) {
+        var user = req.body;
+        try {
+            if (!UserHelper.passwordValidation(user.NewPassword))
+                throw 'Mật khẩu sai định dạng';
+
+            var isOldPassword = UserModel.xuLiLogin(user.TenDangNhap, user.MatKhau);
+
+            if (!isOldPassword)
+                res.json({ "Messenger": "Đã có lỗi xảy ra" });
+            else
+                isOldPassword.then(function (data) {
+                    if (data[0]) {
+                        var result = UserModel.changePassword(user.ID_TaiKhoan, user.NewPassword);
+                        if (!result)
+                            res.json({ "Messenger": "Đã có lỗi xảy ra" });
+                        else
+                            result.then(function (data) {
+                                res.json({ "Messenger": "Thay đổi mật khẩu thành công" });
+                            }).catch(function (err) {
+                                res.json({ "Messenger": "Thông tin nhập vào không chính xác" });
+                            })
+                    }
+                    else {
+                        res.json({ "Messenger": "Mật khẩu cũ không chính xác" });
+                    }
+                }).catch(function (err) {
+                    res.json({ "Messenger": err });
+                })
+
+        }
+        catch (e) {
+            res.json({ "Messenger": e });
+        }
+    });
 module.exports = router;
--- apps/models/user.js
+++ apps/models/user.js
@@ -47,9 +47,40 @@ function uploadAvatar(id, path) {
 
     return false;
 }
+function updateUser(id, user) {
+    if (id && user) {
+        var defer = q.defer();
+        conn.query("UPDATE taikhoan SET ? WHERE ID_TaiKhoan=?",[user,id], function (error, results) {
+            if (error)
+                defer.reject(error);
+            else
+                defer.resolve(results);
+        });
 
+        return defer.promise;
+    }
+
+    return false;
+}
+function changePassword(id, password) {
+    if (id && password) {
+        var defer = q.defer();
+        conn.query("UPDATE taikhoan SET MatKhau=? WHERE ID_TaiKhoan=?",[password,id], function (error, results) {
+            if (error)
+                defer.reject(error);
+            else
+                defer.resolve(results);
+        });
+
+        return defer.promise;
+    }
+
+    return false;
+}
 module.exports = {
     addUser: addUser,
     xuLiLogin: xuLiLogin,
-    uploadAvatar: uploadAvatar
+    uploadAvatar: uploadAvatar,
+    updateUser: updateUser,
+    changePassword: changePassword
 }
